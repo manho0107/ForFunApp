a94fcf5e4e7f408253fe344b8db8b9d48314f7db
Felix Cheung <81549914+manho0107@users.noreply.github.com>
2023-11-03 14:57:19 -0600

    commit;
    For lab 2

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/**
+ * My For Fun App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
